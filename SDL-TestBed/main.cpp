4d5ab8c63485fef4527dbbf854ecd54d40a97849
Artyom Yazinsky <iw2snl@gmail.com>
2018-04-21 02:02:20 +0300

    Return lists

--- SDL-TestBed/main.cpp
+++ SDL-TestBed/main.cpp
@@ -245,14 +245,14 @@ private:
         draw_checkboxes();
         draw_sliders();
         draw_text_entries();
-//        draw_list();
+        draw_list();
     }
 
     Window mButtonsWindow {"Buttons", 30, 30};
     Window mCheckboxesWindow {"Checkboxes", 200, 30};
     Window mSlidersWindow {"Sliders", 430, 30};
     Window mTextEntryWindow {"Text entry", 30, 250};
-//    Window mListWindow {"List", 800, 30};
+    Window mListWindow {"List", 800, 30};
 
     void draw_buttons() {
         primitive::Rectangle rect{40, 0, 100, 30};
@@ -349,57 +349,57 @@ private:
         }).use(mGui.ctx);
     }
 
-//    void draw_list() {
-//        // assume these are members
-//        struct Foo {
-//            const std::string name;
-//        };
-//        static std::vector<Foo> foos {
-//                {"Zero"}, {"One"}, {"Two"}, {"Three"}, {"Four"}, {"Five"},
-//                {"Six"}, {"Seven"}, {"Eight"}, {"Nine"}, {"Ten"}
-//        };
-//        static std::string itemName;
-//
-//        start_window(mListWindow);
-//
-//        primitive::Rectangle rect = {0, 0, 280, 280};
-//        widget::list(
-//                "Test", rect, colors::blue, foos,
-//                [](const Foo& foo) {
-//                    return foo.name.c_str();
-//                },
-//                [](int position, const Foo& foo) {
-//                    std::cout << "Clicked on " << position << "th foo: " << foo.name << '\n';
-//                }
-//        ).use(mGui.ctx);
-//
-//        rect = {rect.x, rect.y + rect.height + 10, rect.width - 120, 40};
-//        widget::entry("Add item", rect, colors::darkGrey, itemName, [](const std::string&) {}).use(mGui.ctx);
-//
-//        rect = {rect.x + rect.width + 5, rect.y, 30, 40};
-//        widget::button{"+", rect, colors::green}.use(mGui.ctx, []() {
-//            if (!itemName.empty()) {
-//                foos.push_back(Foo{itemName});
-//            }
-//        });
-//
-//        rect = {rect.x + rect.width + 5, rect.y, 35, 40};
-//        widget::button{"Cl", rect, colors::violet}.use(mGui.ctx, []() {
-//            itemName.clear();
-//        });
-//
-//        rect = {rect.x + rect.width + 5, rect.y, 35, 40};
-//        widget::button{"+", rect, colors::yellow}.use(mGui.ctx, []() {
-//            foos.push_back(Foo{std::to_string(foos.size())});
-//        });
-//
-//        rect = {0, rect.y + rect.height + 5, 280, 40};
-//        widget::button{"Remove last", rect, colors::red}.use(mGui.ctx, []() {
-//            if (!foos.empty()) {
-//                foos.pop_back();
-//            }
-//        });
-//    }
+    void draw_list() {
+        // assume these are members
+        struct Foo {
+            const std::string name;
+        };
+        static std::vector<Foo> foos {
+                {"Zero"}, {"One"}, {"Two"}, {"Three"}, {"Four"}, {"Five"},
+                {"Six"}, {"Seven"}, {"Eight"}, {"Nine"}, {"Ten"}
+        };
+        static std::string itemName;
+
+        start_window(mListWindow);
+
+        primitive::Rectangle rect = {0, 0, 280, 280};
+        widget::list(
+                "Test", rect, colors::blue, foos,
+                [](const Foo& foo) {
+                    return foo.name.c_str();
+                },
+                [](int position, const Foo& foo) {
+                    std::cout << "Clicked on " << position << "th foo: " << foo.name << '\n';
+                }
+        ).use(mGui.ctx);
+
+        rect = {rect.x, rect.y + rect.height + 10, rect.width - 120, 40};
+        widget::entry("Add item", rect, colors::darkGrey, itemName, [](const std::string&) {}).use(mGui.ctx);
+
+        rect = {rect.x + rect.width + 5, rect.y, 30, 40};
+        if (widget::button{"+", rect, colors::green}.use(mGui.ctx)) {
+            if (!itemName.empty()) {
+                foos.push_back(Foo{itemName});
+            }
+        }
+
+        rect = {rect.x + rect.width + 5, rect.y, 35, 40};
+        if (widget::button{"Cl", rect, colors::violet}.use(mGui.ctx)) {
+            itemName.clear();
+        }
+
+        rect = {rect.x + rect.width + 5, rect.y, 35, 40};
+        if (widget::button{"+", rect, colors::yellow}.use(mGui.ctx)) {
+            foos.push_back(Foo{std::to_string(foos.size())});
+        }
+
+        rect = {0, rect.y + rect.height + 5, 280, 40};
+        if (widget::button{"Remove last", rect, colors::red}.use(mGui.ctx)) {
+            if (!foos.empty()) {
+                foos.pop_back();
+            }
+        }
+    }
 
     void render_frame(int& previousFrameTimestamp) {
         SDL_SetRenderDrawColor(mSdl.renderer, 50, 50, 50, 255);
--- lib/reig/reference_widget_list.h
+++ lib/reig/reference_widget_list.h
@@ -5,66 +5,66 @@
 #include "internal.h"
 #include "reference_widget.h"
 
-//namespace reig::reference_widget {
-//    namespace detail {
-//        // FIXME: This cache can't be dropped
-//        float& get_scroll_value(const void* scrollbar);
-//
-//        template <typename Iter, typename Adapter, typename Action>
-//        struct list {
-//            const char* mTitle = "";
-//            Rectangle mBoundingBox;
-//            Color mBaseColor;
-//            Iter mBegin;
-//            Iter mEnd;
-//            Adapter& mAdapter;
-//            Action& mAction;
-//
-//            void use(Context& ctx) const;
-//        };
-//    }
-//
-//    template <typename Range, typename Adapter, typename Action>
-//    auto list(const char* title, const Rectangle& rectangle, const Color& baseColor,
-//              Range&& range, Adapter&& adapter, Action&& action) {
-//        using std::begin;
-//        using std::end;
-//        return detail::list<decltype(begin(range)), Adapter, Action>
-//                {title, rectangle, baseColor, begin(range), end(range), adapter, action};
-//    };
-//}
-//
-//template <typename Iter, typename Adapter, typename Action>
-//void reig::reference_widget::detail::list<Iter, Adapter, Action>::use(reig::Context& ctx) const {
-//    {
-//        Rectangle listArea = mBoundingBox;
-//        ctx.fit_rect_in_window(listArea);
-//
-//        using namespace colors::operators;
-//        using namespace colors::literals;
-//        ctx.render_rectangle(listArea, internal::get_yiq_contrast(mBaseColor - 50_a));
-//        ctx.render_rectangle(internal::decrease_rect(listArea, 2), mBaseColor - 50_a);
-//    }
-//
-//    auto& scrolled = detail::get_scroll_value(this);
-//    float fontHeight = ctx.get_font_size();
-//    auto skippedItemCount = static_cast<int>(scrolled / fontHeight);
-//
-//    float y = mBoundingBox.y;
-//    float maxY = get_y2(mBoundingBox);
-//    float scrollbarWidth = 30.0f;
-//    for (auto it = mBegin + skippedItemCount; it != mEnd && y < maxY; ++it, y += fontHeight) {
-//        Rectangle itemFrameBox = {mBoundingBox.x + scrollbarWidth, y, mBoundingBox.width, fontHeight};
-//        internal::trim_rect_in_other(itemFrameBox, mBoundingBox);
-//
-//        button{mAdapter(*it), itemFrameBox, mBaseColor}.use(ctx, [*this, it]() {
-//            mAction(it - mBegin, *it);
-//        });
-//    }
-//
-//    auto itemCount = mEnd - mBegin;
-//    Rectangle scrollbarArea {mBoundingBox.x, mBoundingBox.y, scrollbarWidth, mBoundingBox.height};
-//    scrollbar{scrollbarArea, mBaseColor, scrolled, itemCount * fontHeight}.use(ctx);
-//}
+namespace reig::reference_widget {
+    namespace detail {
+        // FIXME: This cache can't be dropped
+        float& get_scroll_value(const void* scrollbar);
+
+        template <typename Iter, typename Adapter, typename Action>
+        struct list {
+            const char* mTitle = "";
+            Rectangle mBoundingBox;
+            Color mBaseColor;
+            Iter mBegin;
+            Iter mEnd;
+            Adapter& mAdapter;
+            Action& mAction;
+
+            void use(Context& ctx) const;
+        };
+    }
+
+    template <typename Range, typename Adapter, typename Action>
+    auto list(const char* title, const Rectangle& rectangle, const Color& baseColor,
+              Range&& range, Adapter&& adapter, Action&& action) {
+        using std::begin;
+        using std::end;
+        return detail::list<decltype(begin(range)), Adapter, Action>
+                {title, rectangle, baseColor, begin(range), end(range), adapter, action};
+    };
+}
+
+template <typename Iter, typename Adapter, typename Action>
+void reig::reference_widget::detail::list<Iter, Adapter, Action>::use(reig::Context& ctx) const {
+    {
+        Rectangle listArea = mBoundingBox;
+        ctx.fit_rect_in_window(listArea);
+
+        using namespace colors::operators;
+        using namespace colors::literals;
+        ctx.render_rectangle(listArea, internal::get_yiq_contrast(mBaseColor - 50_a));
+        ctx.render_rectangle(internal::decrease_rect(listArea, 2), mBaseColor - 50_a);
+    }
+
+    auto& scrolled = detail::get_scroll_value(this);
+    float fontHeight = ctx.get_font_size();
+    auto skippedItemCount = static_cast<int>(scrolled / fontHeight);
+
+    float y = mBoundingBox.y;
+    float maxY = get_y2(mBoundingBox);
+    float scrollbarWidth = 30.0f;
+    for (auto it = mBegin + skippedItemCount; it != mEnd && y < maxY; ++it, y += fontHeight) {
+        Rectangle itemFrameBox = {mBoundingBox.x + scrollbarWidth, y, mBoundingBox.width, fontHeight};
+        internal::trim_rect_in_other(itemFrameBox, mBoundingBox);
+
+        if (button{mAdapter(*it), itemFrameBox, mBaseColor}.use(ctx)) {
+            mAction(it - mBegin, *it);
+        }
+    }
+
+    auto itemCount = mEnd - mBegin;
+    Rectangle scrollbarArea {mBoundingBox.x, mBoundingBox.y, scrollbarWidth, mBoundingBox.height};
+    scrollbar{scrollbarArea, mBaseColor, scrolled, itemCount * fontHeight}.use(ctx);
+}
 
 #endif //REIG_REFERENCE_WIDGET_LIST_H
