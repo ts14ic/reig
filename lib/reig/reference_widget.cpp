a9f57089990bf8344cf60d1cfcbdbc879b47ed9e
Artyom Yazinsky <iw2snl@gmail.com>
2018-04-27 22:56:27 +0300

    Move the base_area rect from ButtonModel to button draw call

--- lib/reig/reference_widget.cpp
+++ lib/reig/reference_widget.cpp
@@ -5,44 +5,42 @@ using namespace reig::primitive;
 
 namespace reig::reference_widget {
     struct ButtonModel {
-        Rectangle outline_area;
-        Rectangle base_area;
-        bool is_hovering_over_area = false;
-        bool has_just_clicked = false;
-        bool is_holding_click = false;
+        const Rectangle bounding_box;
+        const bool is_hovering_over_area = false;
+        const bool has_just_clicked = false;
+        const bool is_holding_click = false;
     };
 
-    ButtonModel get_button_model(Context& ctx, Rectangle outlineArea) {
-        ctx.fit_rect_in_window(outlineArea);
+    ButtonModel get_button_model(Context& ctx, Rectangle outline_area) {
+        ctx.fit_rect_in_window(outline_area);
 
-        ButtonModel model;
-        model.is_hovering_over_area = ctx.mouse.is_hovering_over_rect(outlineArea);
-        model.has_just_clicked = ctx.mouse.left_button.just_clicked_in_rect(outlineArea);
-        model.is_holding_click = model.is_hovering_over_area
-                             && ctx.mouse.left_button.clicked_in_rect(outlineArea)
+        bool is_hovering_over_area = ctx.mouse.is_hovering_over_rect(outline_area);
+        bool has_just_clicked = ctx.mouse.left_button.just_clicked_in_rect(outline_area);
+        bool is_holding_click = is_hovering_over_area
+                             && ctx.mouse.left_button.clicked_in_rect(outline_area)
                              && ctx.mouse.left_button.is_held();
 
-        model.outline_area = outlineArea;
-        model.base_area = model.is_holding_click
-                         ? decrease_rect(outlineArea, 6)
-                         : decrease_rect(outlineArea, 4);
-
-        return model;
+        return {outline_area, is_hovering_over_area, has_just_clicked, is_holding_click};
     }
 
-    bool button(Context& ctx, const char* aTitle, Rectangle bounding_box, Color base_color) {
+    bool button(Context& ctx, const char* title, Rectangle bounding_box, Color base_color) {
         auto model = get_button_model(ctx, bounding_box);
 
         Color inner_color{base_color};
         if (model.is_hovering_over_area) {
             inner_color = colors::lighten_color_by(inner_color, 30);
         }
+        Rectangle base_area;
         if (model.is_holding_click) {
             inner_color = colors::lighten_color_by(inner_color, 30);
+            base_area = decrease_rect(model.bounding_box, 6);
+        } else {
+            base_area = decrease_rect(model.bounding_box, 4);
         }
-        ctx.render_rectangle(model.outline_area, colors::get_yiq_contrast(inner_color));
-        ctx.render_rectangle(model.base_area, inner_color);
-        ctx.render_text(aTitle, model.base_area);
+
+        ctx.render_rectangle(model.bounding_box, colors::get_yiq_contrast(inner_color));
+        ctx.render_rectangle(base_area, inner_color);
+        ctx.render_text(title, base_area);
 
         return model.has_just_clicked;
     }
@@ -55,8 +53,8 @@ namespace reig::reference_widget {
         if (model.is_holding_click || model.is_hovering_over_area) {
             texture = hover_texture;
         }
-        ctx.render_rectangle(model.outline_area, texture);
-        ctx.render_text(title, model.outline_area);
+        ctx.render_rectangle(model.bounding_box, texture);
+        ctx.render_text(title, model.bounding_box);
 
         return model.has_just_clicked;
     }
